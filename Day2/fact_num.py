51ea70d68c5e3d5106706fcb77f2102aafb15af0
RishithaSrija <rishithasrijakasani@gmail.com>
2025-09-10 23:01:30 +0530

    Update fact_num.py

--- Day2/fact_num.py
+++ Day2/fact_num.py
@@ -1,10 +1,12 @@
 def print_fact(n):
     i=1
     f=1
-    while(i<n+1):
-        print(i,"*",end="")
-        f=f*i
-        i+=1
+    for i in range(1,n+1):
+        f*=i
+        if i<n:
+            print(i,"*",end="")
+        else:
+            print(i,end="")
     print("=",f)
 num=int(input("Enter number : "))
 print_fact(num)
