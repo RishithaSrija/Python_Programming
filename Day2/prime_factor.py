c54d336dd8982a263aa291c0c89b245e3475f948
RishithaSrija <rishithasrijakasani@gmail.com>
2025-09-10 22:57:00 +0530

    Update prime_factor.py

--- Day2/prime_factor.py
+++ Day2/prime_factor.py
@@ -1,20 +1,14 @@
 def prime(m):
-    if m==1:
-       pass
-    ct=0
-    for i in range(1,m+1):
+    if m<2:
+       return False
+    for i in range(2,int(m**0.5)+1):
         if(m%i==0):
-            ct+=1
-    if(ct==2):
-           return m
-    else:
-            pass
+           return False
+    return True
 def prime_fact(n):
-    s=0
-    for i in range(1,n+1):
-        if(n%i==0):
-           val=prime(i)
-           print(val, end=" ")
+    for i in range(2,n+1):
+        if(n%i==0)and prime(i):
+           print(i, end=" ")
 
 num=int(input("Enter a number : "))        
 prime_fact(num)
