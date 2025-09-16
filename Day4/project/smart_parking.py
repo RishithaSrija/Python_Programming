4f42758fa5718f4d961016a6be27afeb6c11370f
RishithaSrija <rishithasrijakasani@gmail.com>
2025-09-16 20:05:14 +0530

    Update smart_parking.py

--- Day4/project/smart_parking.py
+++ Day4/project/smart_parking.py
@@ -1,108 +1,191 @@
-# class vehicle:
+from abc import ABC,abstractmethod
+
+class vehicle:
+    def __init__(self, vid, license_plate, owner_name):
+        self.vid = vid
+        self.__license_plate = license_plate
+        self.__owner_name = owner_name
+        self.__spot_status = None
        
-#     def getlicense(self):
-#         return self.__license_plate
-#     def setlicence(self,lic_num,name,spot):
-#         self.__license_plate=lic_num
-#     def getowner(self):
-#         return self.__owner_name
-#     def setowner(self,name):
-#         self.__owner_name=name
-#     def getspot(self):
-#         return self.__spot_status
-#     def setspot(self,spot):
-#         self.__spot_status=spot 
-
-
-
-
-# class Bike(vehicle):
-
-#     def gethelmet(self):
-#         return self.helmet
-#     def sethelmet(self,h):
-#         self.helmet=h
-#     def display(self):
-#         print("Bike Details: ")
-#         print("Lincense_plate of bike:  ",self.getlicense(),"\nOwner name:  ",self.getowner,"\nSpot Status:  ",self.getspot)
-#     def calculate_parking_fee(hrs):
-#         print("Parking fee for Bike: ₹",20*hrs)
-
-# class Car(vehicle):
+    def getlicense(self):
+        return self.__license_plate
+    def setlicence(self,lic_num,name,spot):
+        self.__license_plate=lic_num
+    def getowner(self):
+        return self.__owner_name
+    def setowner(self,name):
+        self.__owner_name=name
+    def getspot(self):
+        return self.__spot_status
+    def setspot(self,spot):
+        self.__spot_status=spot
+     
+    def display(self):
+       
+        print(f"Vehicle ID:,{self.vid},Lincense_plate of bike:  ,{self.__license_plate},\nOwner name:  ,{self.__owner_name}")
     
-#     def getseats(self):
-#         return self.seats
-#     def setseats(self,s):
-#         self.seats=s
-#     def display(self):
-#         print("Car Details: ")
-#         print("Lincense_plate of car:  ",self.getlicense(),"\nOwner name:  ",self.getowner,"\nSpot Status:  ",self.getspot)
-#     def  calculate_parking_fee(hrs):
-#         print("Parking fee for Car: ₹",50*hrs)
-# class SUV(vehicle):
+    def calculate_parking_fee(self,hrs):
 
-#     def getfour_wheel(self):
-#         return self.four_wheel
-#     def setfour_wheel(self,w):
-#         self.four_wheel=w
-#     def display(self):
-#         print("SUV Details: ")
-#         print("Lincense_plate of SUV:  ",self.getlicense(),"\nOwner name:  ",self.getowner,"\nSpot Status:  ",self.getspot)
-#     def  calculate_parking_fee(hrs):
-#         print("Parking fee for SUV: ₹",70*hrs)
-# class Truck(vehicle):
-
-#     def getmax_load(self):
-#         return self.max_load
-#     def setmax_load(self,c):
-#         self.max_load=c
-#     def display(self):
-#         print("Truck Details: ")
-#         print("Lincense_plate of truck:  ",self.getlicense(),"\nOwner name:  ",self.getowner,"\nSpot Status:  ",self.getspot)
-#     def  calculate_parking_fee(hrs):
-#         print("Parking fee for Truck: ₹",100*hrs)
-
-# vehicles=[Bike(),Car(),SUV(),Truck()]
-# for v in vehicles:
-#     v.display()
-
-# class payment:
-#     @abstractmethod
-#     def process_payment(self,amount):
-#         self.amount=amount
-
-# class cashPay(payment):
-#     def process_payment(self,amount):
-#         print(f"Paid ₹{amount} in cash")
-# class cardPay(payment):
-#     def process_payment(self,amount):
-#         print(f"Paid ₹{amount} using credit/debit card")
-# class upiPay(payment):
-#      def process_payment(self,amount):
-#         print(f"Paid ₹{amount} using UPI")
-
-# payments = [cashPay(), cardPay(), upiPay()]
-# for p in payments:
-#     p.pay(1000)
+        raise NotImplementedError("Subclasses must override")
 
 
+class Bike(vehicle):
+
+    def __init__(self, vid, license_plate, owner_name,helmet_req):
+        super().__init__(vid,license_plate,owner_name)
+        self.helmet=helmet_req
+    def display(self):
+        print("Bike Details: ")
+        print(f"Lincense_plate of bike:  {self.getlicense()} Owner name:  {self.getowner()} Spot Status:  {self.getspot()} Helmet: {self.helmet}")
+    def calculate_parking_fee(self,hrs):
+        print("Parking fee for Bike: ₹",end="")
+        return 20*hrs
+
+class Car(vehicle):
+    
+    def __init__(self, vid, license_plate, owner_name,seats):
+        super().__init__(vid,license_plate,owner_name)
+        self.seats=seats
+    def display(self):
+        print("Car Details: ")
+        print("Lincense_plate of car:  ",self.getlicense(),"\nOwner name:  ",self.getowner(),"\nSpot Status:  ",self.getspot(),"Seats: ",self.seats)
+    def  calculate_parking_fee(self,hrs):
+        print("Parking fee for Car: ₹",end='')
+        return 50*hrs
+class SUV(vehicle):
+
+    def __init__(self, vid, license_plate, owner_name,four_wheel):
+        super().__init__(vid,license_plate,owner_name)
+        self.four_wheel=four_wheel
+    def display(self):
+        print("SUV Details: ")
+        print("Lincense_plate of SUV:  ",self.getlicense(),"\nOwner name:  ",self.getowner(),"\nSpot Status:  ",self.getspot(),"4-Wheel-drive: ",self.four_wheel)
+    def  calculate_parking_fee(self,hrs):
+        print("Parking fee for SUV: ₹",end='')
+        return 70*hrs
+class Truck(vehicle):
+
+    def __init__(self, vid, license_plate, owner_name,max_load):
+        super().__init__(vid,license_plate,owner_name)
+        self.max_load=max_load
+    def display(self):
+        print("Truck Details: ")
+        print("Lincense_plate of truck:  ",self.getlicense(),"\nOwner name:  ",self.getowner(),"\nSpot Status:  ",self.getspot(),"Maximum Load: ",self.max_load)
+    def  calculate_parking_fee(self,hrs):
+        print("Parking fee for Truck: ₹",end='')
+        return 100*hrs
 
 
-# bike1 = Bike("B101", "TS01AB1234", "Rahul", True)
-# car1 = Car("C201", "TS05CD5678", "Priya", 5)
-# suv1 = SUV("S301", "TS09EF9012", "Anjali", True)
-# truck1 = Truck("T401", "TS11XY4455", "Ravi", 12)
+class payment(ABC):
+    @abstractmethod
+    def process_payment(self,amount):
+        pass
 
-# #task-2
-# class parkingSpot:
-#     def assign_vehicle():
-#     def remove_vehicle():
+class cashPay(payment):
+    def process_payment(self,amount):
+        print(f"Paid ₹{amount} in cash")
+class cardPay(payment):
+    def process_payment(self,amount):
+        print(f"Paid ₹{amount} using credit/debit card")
+class upiPay(payment):
+     def process_payment(self,amount):
+        print(f"Paid ₹{amount} using UPI")
+
+#task-2
+class parkingSpot:
+    def __init__(self,spot_id,size):
+        self.spot_id=spot_id
+        self.size=size
+        self.__vehicle=None
+    def assign_vehicle(self,vehicle):
+        if self.__vehicle is None:
+            self.__vehicle=vehicle
+            vehicle.setspot(self.spot_id)
+            return True
+        return False
+    def remove_vehicle(self):
+        v=self.__vehicle
+        if v:
+            self.__vehicle=None
+            v.setspot(None)
+        return v
+    def isEmpty(self):
+        return self.__vehicle is None
+    def getvehicle(self):
+        return self.__vehicle
+
+    def show_status(self):
+        if self.__vehicle:
+            print(f"Vehicle status:\n Spot: {self.spot_id} ({self.size}):Occupied->{type(self.__vehicle).__name__} ({self.__vehicle.getlicense()})")
+        else:
+             print(f"Vehicle status:\n Spot: {self.spot_id} ({self.size}):Empty")
          
-# #task-3 
+#task-3 
 
-# class parkingLot:
-#     def add_spot():
-#     def show_spot():
-#     def park_veh(veh):
-#     def unpark_veh(veh):
-# #Payment
+class parkingLot:
+    def __init__(self,name):
+        self.name=name
+        self.spots=[]
+
+    def add_spot(self,spot):
+        self.spots.append(spot)
+
+    def show_spot(self):
+        print("\nParking Status: ")
+        for s in self.spots:
+            s.show_status()
+
+    def park_veh(self,veh):
+        size_req={
+            Bike:["S","M","L","XL"],
+            Car:["M","L","XL"],
+            SUV:["L","XL"],
+            Truck:["XL"]
+        }
+        for s in self.spots:
+            if s.size in size_req[type(veh)] and s.isEmpty():
+                s.assign_vehicle(veh)
+                return True
+        print("No suitable spot to park the vehicle",type(veh).__name__)
+        return False
+
+    def unpark_veh(self,veh,hours,pay_method:payment):
+        for s in self.spots:
+            if not s.isEmpty() and s.getvehicle()==veh:
+                s.remove_vehicle()
+                fee=veh.calculate_parking_fee(hours)
+                print(f"{type(veh).__name__} ({veh.getlicense()}) removed from parking spot {s.spot_id}")
+                print(f"Parking fee Calculated =₹{fee}")
+                pay_method.process_payment(fee)
+                return
+        print("Vehicle not found in lot")
+
+
+if __name__=="__main__":
+    lot=parkingLot("CityMall Parking")
+    lot.add_spot(parkingSpot(1,'S'))
+    lot.add_spot(parkingSpot(2,'M'))
+    lot.add_spot(parkingSpot(3,'S'))
+    lot.add_spot(parkingSpot(4,'L'))
+    lot.show_spot()
+    lot.add_spot(parkingSpot(5,'L'))
+    lot.add_spot(parkingSpot(6,'XL'))
+
+
+    print(f"Parking Lot :{lot.name} Total Spots Added: {len(lot.spots)}")
+
+    bike1 = Bike("B101", "TS01AB1234", "Rahul", True)
+    car1 = Car("C201", "TS05CD5678", "Priya", 5)
+    suv1 = SUV("S301", "TS09EF9012", "Anjali", True)
+    truck1 = Truck("T401", "TS11XY4455", "Ravi", 12)
+
+    lot.park_veh(bike1)
+    lot.park_veh(car1)
+    lot.park_veh(suv1)
+    lot.park_veh(truck1)
+
+    lot.show_spot()
+    lot.unpark_veh(car1,hours=6,pay_method=upiPay())
+
+    lot.show_spot()
+
